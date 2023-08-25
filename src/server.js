c39a18bef375ae34894824de5055777c3141539c
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2023-08-25 14:40:13 +0700

    init code

--- src/server.js
+++ src/server.js
@@ -1,12 +1,30 @@
 import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
 const app = express()
 
 const hostname = 'localhost'
-const port = 8000
+const port = 8017
 
-app.get('/', (req, res) => res.send('<h1>Welcome to my project</h1>'))
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  console.log(
+    mapOrder(
+      [
+        { id: 'id-1', name: 'One' },
+        { id: 'id-2', name: 'Two' },
+        { id: 'id-3', name: 'Three' },
+        { id: 'id-4', name: 'Four' },
+        { id: 'id-5', name: 'Five' }
+      ],
+      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+      'id'
+    )
+  )
+  res.end('<h1>Hello World!</h1><hr>')
+})
 
-app.listen(port, hostname, () =>
-  console.log(`Hello, I'm running at: http://${hostname}:${port}`)
-)
+app.listen(port, hostname, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Hello Trung Quan Dev, I am running at ${hostname}:${port}/`)
+})
