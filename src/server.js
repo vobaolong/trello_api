5828d4567f668a4969cf1d697a39e9daade918b8
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2023-09-11 23:49:41 +0700

    fix env

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import express from 'express'
-import { CONNECT_DB, GET_DB } from '@/config/mongodb'
+import { CONNECT_DB } from './config/mongodb.js'
 
 const START_SERVER = () => {
   const app = express()
@@ -13,13 +13,17 @@ const START_SERVER = () => {
   })
 
   app.listen(port, hostname, () => {
-    console.log(`Hello BaoLong Dev, I'm running at http://${hostname}:${port}/`)
+    console.log(
+      `3. Hello BaoLong Dev 🎉, I'm running at http://${hostname}:${port}/ `
+    )
   })
 }
+
+console.log('1. Connecting to MongoDB Cloud Atlas...')
 CONNECT_DB()
-  .then(() => console.log('Connect ok'))
+  .then(() => console.log('2. Connected to MongoDB Cloud Atlas✅'))
   .then(() => START_SERVER())
-  .catch((err) => {
-    console.error(err)
+  .catch((error) => {
+    console.error(error)
     process.exit(0)
   })
