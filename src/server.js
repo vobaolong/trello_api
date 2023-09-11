0cfcf72a6af282faf381ac05f31bb1c2d4de2b5f
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2023-09-11 22:33:49 +0700

    feat connect to db

--- src/server.js
+++ src/server.js
@@ -1,30 +1,13 @@
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import 'dotenv/config'
 
 const app = express()
-
-const hostname = 'localhost'
-const port = 8017
-
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(
-    mapOrder(
-      [
-        { id: 'id-1', name: 'One' },
-        { id: 'id-2', name: 'Two' },
-        { id: 'id-3', name: 'Three' },
-        { id: 'id-4', name: 'Four' },
-        { id: 'id-5', name: 'Five' }
-      ],
-      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-      'id'
-    )
-  )
-  res.end('<h1>Hello World!</h1><hr>')
+const PORT = process.env.PORT
+const HOST = process.env.HOST
+app.use('/', (req, res) => {
+  res.send('<h1>Hello World!</h1><hr>')
 })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${hostname}:${port}/`)
+app.listen(HOST, PORT || 8080, () => {
+  console.log(`Hello BaoLong Dev, I'm running at ${HOST}:${PORT}/`)
 })
