d9d663bf7dc11b3b5828b8608c5aab3ec1a28213
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2023-09-15 21:54:23 +0700

    done

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,15 +1,12 @@
 // baolong01dev
 // rU8aBT4ItqGFOesx
-const MONGODB_URI =
-  'mongodb+srv://baolong01dev:rU8aBT4ItqGFOesx@cluster0-baolongdev.ti1711u.mongodb.net/?retryWrites=true&w=majority'
-const DATABASE_NAME = 'trello-baolongdev'
 
 import { MongoClient, ServerApiVersion } from 'mongodb'
-
+import { env } from './environment.js'
 let trelloDatabaseInstance = null
 
 // khởi tạo đối tượng mongoClientInstance để connect tới MongoDB
-const mongoClientInstance = new MongoClient(MONGODB_URI, {
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -21,7 +18,7 @@ const mongoClientInstance = new MongoClient(MONGODB_URI, {
 export const CONNECT_DB = async () => {
   await mongoClientInstance.connect()
 
-  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
 }
 
 // GET_DB kh async có nhiệm vụ export ra trello database instance sau khi đã connect thành công với MongoDB để chúng ta sử dụng ở nhiều nơi khác nhau trong code, lưu ý phải đảm bảo chỉ luôn gọi getDB này sau khi đã kết nối thành công tới mongoDB
