de6e243d8c7cb77fed2d08972c23130fec461f31
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2023-10-12 11:38:52 +0700

    done

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,7 +1,7 @@
+import Joi from 'joi'
+import { ObjectId } from 'mongodb'
 import { GET_DB } from '~/config/mongodb'
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
-
-const Joi = require('joi')
 
 const BOARD_COLLECTION_NAME = 'boards'
 const BOARD_COLLECTION_SCHEMA = Joi.object({
@@ -16,8 +16,16 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false)
 })
 
+const validateBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, {
+    abortEarly: false
+  })
+}
+
 const createNew = async (data) => {
   try {
+    const validData = await validateBeforeCreate(data)
+    console.log(validData)
     const createdBoard = await GET_DB()
       .collection(BOARD_COLLECTION_NAME)
       .insertOne(data)
@@ -29,9 +37,11 @@ const createNew = async (data) => {
 
 const findOneById = async (id) => {
   try {
-    const result = await GET_DB().collection(BOARD_COLLECTION_NAME).findOne({
-      _id: id
-    })
+    const result = await GET_DB()
+      .collection(BOARD_COLLECTION_NAME)
+      .findOne({
+        _id: new ObjectId(id)
+      })
     return result
   } catch (error) {
     throw new Error(error)
